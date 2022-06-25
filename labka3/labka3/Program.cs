169107326e19ce98c2b6b7a98c488327a6d92aa7
Oleninkaa <olyarosnovska@gmail.com>
2022-06-25 14:55:13 +0200

    lab3

---
+++ labka3/labka3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Lab_3
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader File = new StreamReader("D:\\2 semestr\\прога\\labki\\labka3\\labka3\\read.txt");
+                List<string> list = new List<string> { };
+                while (!File.EndOfStream)
+                {
+                    list.Add(File.ReadLine());
+                }
+                File.Close();
+
+                list.Reverse();
+                StreamWriter File2 = new StreamWriter("D:\\2 semestr\\прога\\labki\\labka3\\labka3\\write.txt");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    File2.WriteLine(list[i]);
+                }
+                File2.Close();
+                Console.WriteLine("Рядки успiшно записанi у файл write.txt");
+            }
+            catch { Console.WriteLine("Помилка"); }
+        }
+    }
+}
---
+++ labka3/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 78, 11, 13, 5, 225 };
+            int tvir = list.Aggregate(seed: 1, func: (result, item) => result * (item % 10)); 
+            Console.WriteLine("Твiр останнiх цифр елементiв послiдовностi: " + tvir);
+        }
+    }
+}
